7d1146014af1d6f12add64bb841a8cfeeb6a8cc8
n.friehs <noah.friehs@edu.fh-joanneum.at>
2023-12-12 14:58:53 +0100

    Add AppTest class for CSV input validation

---
+++ src/test/java/at/md/AppTest.java
@@ -0,0 +1,36 @@
+package at.md;
+
+import at.md.General.Application;
+import org.testng.annotations.Test;
+
+import java.io.*;
+
+import static org.testng.Assert.assertEquals;
+
+public class AppTest {
+
+
+    @Test
+    public void test_promt_user_enter_csv() throws IOException {
+        System.out.println("AppTest.test_promt_user_enter_csv");
+        File tmpFile = File.createTempFile("tmp", ".csv");
+
+        String expected = tmpFile.getAbsolutePath();
+
+        InputStream inputStream = new ByteArrayInputStream(expected.getBytes());
+        InputStream originalIn = System.in;
+        PrintStream originalOut = System.out;
+
+        try {
+            System.setIn(inputStream);
+
+            assertEquals(Application.getDatapath(), expected);
+        } finally {
+            System.setIn(originalIn);
+            System.setOut(originalOut);
+            tmpFile.deleteOnExit();
+        }
+    }
+
+}
+
